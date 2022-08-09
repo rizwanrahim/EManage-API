cff8a743978c19bd655460cab2558604d8a4e0c7
Rizwan Rahim <rizwanrahim@live.com>
2022-08-09 13:00:16 +0530

    refactor user role validation

--- src/main/java/com/validators/AppValidator.java
+++ src/main/java/com/validators/AppValidator.java
@@ -1,5 +1,6 @@
 package com.validators;
 
+import com.generic.AppResponse;
 import lombok.Getter;
 import org.springframework.stereotype.Component;
 
@@ -9,4 +10,16 @@ import java.util.List;
 @Component @Getter
 public abstract class AppValidator<T> implements IAppValidator<T> {
     protected final List<String> errors = new ArrayList<>();
+    protected <Res extends AppResponse> Res SetResponse(Res res) {
+        if(!errors.isEmpty()) {
+            res.setValidationMessage(errors);
+            res.setValidated(false);
+            res.setSucceded(false);
+            return res;
+        }
+
+        res.setValidated(true);
+        res.setSucceded(true);
+        return res;
+    }
 }
